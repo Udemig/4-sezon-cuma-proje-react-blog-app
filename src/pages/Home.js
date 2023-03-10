143c3686f6f252cd31dda11615dc0eb52bbda81d
emreharman <emrehrmnb@gmail.com>
2023-03-10 23:58:06 +0300

    loginStructure&listCategories

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
+import { useSelector } from "react-redux";
 
+import Header from "../components/Header";
+import CategoriesSection from "../components/CategoriesSection";
 
-const Home=()=>{
-    return(
-        <div>
-            <h1>ansayfa</h1>
-        </div>
-    )
-}
+const Home = () => {
+  const { loginState,blogsState } = useSelector((state) => state);
+  return (
+    <div>
+      <Header />
+      <main className="mainContainer">
+        <CategoriesSection />
+        <section className="rightSide"></section>
+      </main>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
